26c14ea5ee54db8085741f1ff9c5246e573c2d1d
Corne <39339251+corne12345@users.noreply.github.com>
2018-11-09 20:05:21 +0100

    Initial

--- app/src/main/java/com/example/corne/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/corne/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.corne.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+
+
+    }
+
+    public void checkboxClicked(View view) {
+    }
+
+    public void resetClicked(View view) {
+
     }
 }
