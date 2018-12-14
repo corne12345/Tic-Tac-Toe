955d53b849af72e32d445db2f5ac9119443f7b6b
Corne <39339251+corne12345@users.noreply.github.com>
2018-12-14 15:00:34 +0100

    UPDATE

--- app/src/main/java/com/example/corne/tictactoe/Game.java
+++ app/src/main/java/com/example/corne/tictactoe/Game.java
@@ -1,28 +1,31 @@
 package com.example.corne.tictactoe;
 
+import android.widget.Toast;
+
 import java.io.Serializable;
 
 public class Game implements Serializable {
     final private int BOARD_SIZE = 3;
-    private TileState [][] board;
-
+    private TileState[][] board;
     private Boolean playerOneTurn;
     private int movesPlayed;
     private Boolean gameOver;
 
     public Game() {
         board = new TileState[BOARD_SIZE][BOARD_SIZE];
-        for (int i = 0; i < BOARD_SIZE; i++)
-            for (int j = 0; j < BOARD_SIZE; j++)
+        for(int i=0; i<BOARD_SIZE; i++)
+            for(int j=0; j<BOARD_SIZE; j++)
                 board[i][j] = TileState.BLANK;
 
         playerOneTurn = true;
         gameOver = false;
     }
+
     public TileState choose(int row, int column) {
-        TileState currentBox = board[row][column];
-        if (currentBox == TileState.BLANK) {
-            if (playerOneTurn == true) {
+        movesPlayed += 1;
+        TileState current = board[row][column];
+        if(current == TileState.BLANK){
+            if (playerOneTurn){
                 board[row][column] = TileState.CROSS;
                 playerOneTurn = false;
                 return TileState.CROSS;
@@ -35,7 +38,10 @@ public class Game implements Serializable {
             return TileState.INVALID;
         }
     }
-    public GameState won() {
+
+    public GameState won(){
+
+        // check if player one has won
         if (board[0][0] == TileState.CROSS & board[0][1] == TileState.CROSS & board[0][2] == TileState.CROSS){
             return GameState.PLAYER_ONE;
         }
@@ -82,8 +88,11 @@ public class Game implements Serializable {
         else if (board[0][0] == TileState.CIRCLE & board[1][1] == TileState.CIRCLE & board[2][2] == TileState.CIRCLE){
             return GameState.PLAYER_TWO;
         }
-        else if (board[0][2] == TileState.CIRCLE & board[1][1] == TileState.CIRCLE & board[2][0] == TileState.CIRCLE){
+        else if (board[0][2] == TileState.CIRCLE & board[1][1] == TileState.CIRCLE & board[2][0] == TileState.CIRCLE) {
             return GameState.PLAYER_TWO;
+        }
+        else if (movesPlayed == 9){
+            return GameState.DRAW;
         }
 
         return GameState.IN_PROGRESS;
--- app/src/main/java/com/example/corne/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/corne/tictactoe/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.corne.tictactoe;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.xml.datatype.Duration;
 
 public class MainActivity extends AppCompatActivity {
 
-    Game game;
+    Game mGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,166 +22,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null){
-            game = new Game();
+            mGame = new Game();
         }
-        else{
-            game = (Game) savedInstanceState.getSerializable("gameState");
-
-            String state_tile = (String) savedInstanceState.getSerializable("button1");
-            TextView text_tile = findViewById(R.id.button1);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button2");
-            text_tile = findViewById(R.id.button2);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button3");
-            text_tile = findViewById(R.id.button3);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button4");
-            text_tile = findViewById(R.id.button4);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button5");
-            text_tile = findViewById(R.id.button5);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button6");
-            text_tile = findViewById(R.id.button6);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button7");
-            text_tile = findViewById(R.id.button7);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button8");
-            text_tile = findViewById(R.id.button8);
-            text_tile.setText(state_tile);
-            state_tile = (String) savedInstanceState.getSerializable("button9");
-            text_tile = findViewById(R.id.button9);
-            text_tile.setText(state_tile);
-            GameState check = game.won();
-            disableButtons(check);
+        else {
+            mGame = (Game) savedInstanceState.getSerializable("gameState");
+            for (int i = 0; i < 9; i++) {
+                String buttonString = "button" + i;
+                int loopID = getResources().getIdentifier(buttonString, "id", getPackageName());
+                Button button = findViewById(loopID);
+                String tileState = (String) savedInstanceState.getSerializable(buttonString);
+                if (tileState.equals("x")) {
+                    button.setBackgroundResource(R.drawable.x);
+                } else if (tileState.equals("o")) {
+                    button.setBackgroundResource(R.drawable.o);
+                }
+            }
         }
-
     }
 
-    // Reset the game when the reset button is pressed
-    public void resetClicked(View view) {
-        game = new Game();
-        setContentView(R.layout.activity_main);
-    }
-
-    public void tileClicked(View view) {
+    public void tileClicked (View view){
         int id = view.getId();
         int row = 0;
         int column = 0;
-        switch (id){
-            case R.id.button1:
-                row = 0;
-                column = 0;
-                break;
-            case R.id.button2:
-                row = 0;
-                column = 1;
-                break;
-            case R.id.button3:
-                row = 0;
-                column = 2;
-                break;
-            case R.id.button4:
-                row = 1;
-                column = 0;
-                break;
-            case R.id.button5:
-                row = 1;
-                column = 1;
-                break;
-            case R.id.button6:
-                row = 1;
-                column = 2;
-                break;
-            case R.id.button7:
-                row = 2;
-                column = 0;
-                break;
-            case R.id.button8:
-                row = 2;
-                column = 1;
-                break;
-            case R.id.button9:
-                row = 2;
-                column = 2;
-                break;
+        for (int i = 0; i < 9; i++){
+            String button = "button"+i;
+            int loopID = getResources().getIdentifier(button, "id", getPackageName());
+            if (loopID == id){
+                row = i/3;
+                column = i % 3;
+            }
         }
-        TileState state = game.choose(row, column);
+        TileState state = mGame.choose(row, column);
+        Button button = findViewById(id);
+
         switch(state){
             case CROSS:
-                TextView button = findViewById(id);
-                button.setText("X");
+                button.setBackgroundResource(R.drawable.x);
+                button.setText("x");
                 break;
             case CIRCLE:
-                button = findViewById(id);
-                button.setText("O");
+                button.setBackgroundResource(R.drawable.o);
+                button.setText("o");
                 break;
             case INVALID:
                 break;
         }
-
-        GameState check = game.won();
+        GameState check = mGame.won();
         disableButtons(check);
     }
-    public void disableButtons(GameState check){
-        if (check == GameState.PLAYER_ONE || check == GameState.PLAYER_TWO || check == GameState.DRAW){
-            Button button1 = findViewById(R.id.button1);
-            button1.setEnabled(false);
-            Button button2 = findViewById(R.id.button2);
-            button2.setEnabled(false);
-            Button button3 = findViewById(R.id.button3);
-            button3.setEnabled(false);
-            Button button4 = findViewById(R.id.button4);
-            button4.setEnabled(false);
-            Button button5 = findViewById(R.id.button5);
-            button5.setEnabled(false);
-            Button button6 = findViewById(R.id.button6);
-            button6.setEnabled(false);
-            Button button7 = findViewById(R.id.button7);
-            button7.setEnabled(false);
-            Button button8 = findViewById(R.id.button8);
-            button8.setEnabled(false);
-            Button button9 = findViewById(R.id.button9);
-            button9.setEnabled(false);
+
+    public void disableButtons(GameState check) {
+        if (check == GameState.PLAYER_ONE){
+            Toast.makeText(this, "Player one won", Toast.LENGTH_LONG).show();
+        }
+        else if (check == GameState.PLAYER_TWO){
+            Toast.makeText(this, "Player two won", Toast.LENGTH_LONG).show();
+        }
+        if (check != GameState.IN_PROGRESS){
+            for (int i = 0; i < 9; i ++){
+                String buttonString = "button"+i;
+                int loopID = getResources().getIdentifier(buttonString, "id", getPackageName());
+                Button button = findViewById(loopID);
+                button.setEnabled(false);
+            }
         }
     }
 
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        TextView state1 = findViewById(R.id.button1);
-        String save1 = state1.getText().toString();
-        outState.putSerializable("button1", save1);
-        TextView state2 = findViewById(R.id.button2);
-        String save2 = state2.getText().toString();
-        outState.putSerializable("button2", save2);
-        TextView state3 = findViewById(R.id.button3);
-        String save3 = state3.getText().toString();
-        outState.putSerializable("button3", save3);
-        TextView state4 = findViewById(R.id.button4);
-        String save4 = state4.getText().toString();
-        outState.putSerializable("button4", save4);
-        TextView state5 = findViewById(R.id.button5);
-        String save5 = state5.getText().toString();
-        outState.putSerializable("button5", save5);
-        TextView state6 = findViewById(R.id.button6);
-        String save6 = state6.getText().toString();
-        outState.putSerializable("button6", save6);
-        TextView state7 = findViewById(R.id.button7);
-        String save7 = state7.getText().toString();
-        outState.putSerializable("button7", save7);
-        TextView state8 = findViewById(R.id.button8);
-        String save8 = state8.getText().toString();
-        outState.putSerializable("button8", save8);
-        TextView state9 = findViewById(R.id.button9);
-        String save9 = state9.getText().toString();
-        outState.putSerializable("button9", save9);
-
-        outState.putSerializable("gameState", game);
+    public void resetClicked(View view){
+        mGame = new Game();
+        setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        for (int i = 0; i < 9; i ++) {
+            String buttonString = "button" + i;
+            int loopID = getResources().getIdentifier(buttonString, "id", getPackageName());
+            Button button = findViewById(loopID);
+            String save = button.getText().toString();
+            Log.e("TESTTEST", save);
+            outState.putSerializable(buttonString, save);
+        }
+        outState.putSerializable("gameState", mGame);
+    }
 }
